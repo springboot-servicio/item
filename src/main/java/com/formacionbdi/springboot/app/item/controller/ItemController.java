964e98e03781adccff99c79e2993b0c898a0b310
iscipar <iscipar@hotmail.com>
2020-03-03 13:35:23 +0100

    Se añade el refresco a través de Actuator

--- src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
 	
