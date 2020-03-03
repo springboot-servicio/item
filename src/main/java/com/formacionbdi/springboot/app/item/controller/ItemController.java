ebef4a895232a11998905c9b4684b5c7666cfbf5
iscipar <iscipar@hotmail.com>
2020-03-03 12:53:31 +0100

    Se configura un profile

--- src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,9 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 public class ItemController {
 	
 	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -63,6 +67,10 @@ public class ItemController {
 		Map<String, String> json = new HashMap<>();
 		json.put("entorno", entorno);
 		json.put("puerto", puerto);
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
