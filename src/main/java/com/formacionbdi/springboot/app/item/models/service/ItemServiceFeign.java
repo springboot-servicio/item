49a8da331a906f4048b78844fc850191dea884ed
iscipar <iscipar@hotmail.com>
2020-03-04 12:39:30 +0100

    Se añade el CRUD

--- src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceFeign.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceFeign.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.formacionbdi.springboot.app.item.clientes.ProductoClienteRest;
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 
 @Service("serviceFeign")
 public class ItemServiceFeign implements ItemService {
@@ -23,5 +24,20 @@ public class ItemServiceFeign implements ItemService {
 	@Override
 	public Item findById(Long id, Integer cantidad) {
 		return new Item(clienteFeign.detalle(id), cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		return clienteFeign.crear(producto);
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+		return clienteFeign.editar(producto, id);
+	}
+
+	@Override
+	public void delete(Long id) {
+		clienteFeign.eliminar(id);
 	}
 }
