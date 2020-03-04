f47fd8fb6e7ef8232de1a9bcdd8ea5977f1ac292
iscipar <iscipar@hotmail.com>
2020-03-04 16:27:50 +0100

    Se utiliza el servicio commons

--- src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
-import com.formacionbdi.springboot.app.item.models.Producto;
+import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
